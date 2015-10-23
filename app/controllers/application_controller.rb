4b9c6d19bec07cabfb1b5f678c0b64a4dbb6eda0
paras <rockypro92@gmail.com>
2015-10-23 14:58:18 +0530

    New method and final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   def hello
     render text: "hello, world!"
-  end  
+  end
+  
+  def goodbye
+    render text: "goodbye, world!"
+  end    
 end
 
